6387011e7aa7791e0bc76869e2703072c204a88b
someSkunkFunk <dersfocused@gmail.com>
2024-01-23 17:23:39 -0500

    edited noodle which is now basically a timestamp comparison/plotting script (because trf results don't seem very good)

--- code/noodle.py
+++ code/noodle.py
@@ -3,16 +3,11 @@ import utils
 import scipy.io as spio
 import os
 import matplotlib.pyplot as plt
+import pickle
+import numpy as np
 
 #%%
-subj_num="3315"
-evnt_path=os.path.join("..", "eeg_data", "timestamps", f"evnt_{subj_num}.mat" )
-evnt_mat=spio.loadmat(evnt_path)
-
-# returns dict for some reason, which mat2dict doesnt like
-evnt=evnt_mat['evnt']
-mystery=utils.mat2dict(evnt)
-
+subj_num="3326"
 # get test subject raw data
 #TODO: update evnt_path so it reflects test subject
 
@@ -27,23 +22,51 @@ stims_dict=utils.get_stims_dict(stims_pth)
 #%%
 # select stim to plot
 stim_index=0
-
-# NOTE: not sure if names will certainly be in order
-
-# get using weird indexing because object ndarray
-stim_nm=mystery['name'][0, stim_index][0]
-sync_position=mystery['syncPosition'][0,stim_index][0,0]
-
-stim_wav=utils.get_stim_wav(stims_dict,stim_nm,'clean')
-
-# get eeg sound recording 
-#TODO: get block from stim_nm
-fs_audio=stims_dict['fs'][0]
-stim_dur=(stim_wav.size-1)/fs_audio
-#TODO: add eeg_fs info to subj eeg?
 fs_eeg=2400
-block=stim_nm[:3].capitalize().replace('0','')
-eeg_wav=subj_eeg[block][sync_position:sync_position+int(stim_dur*fs_eeg+1),-1]
+#TODO: get block from selected_stim_nm
+fs_audio=stims_dict['fs'][0]
+
+#which timestamps to use: evnt or mine
+choose_timestamps="mine"
+#use timestamps provided in evnt structure
+if choose_timestamps=="evnt":
+    # NOTE: not sure if names will certainly be in order
+    evnt_path=os.path.join("..", "eeg_data", "timestamps", f"evnt_{subj_num}.mat" )
+    evnt_mat=spio.loadmat(evnt_path)
+    # returns dict for some reason, which mat2dict doesnt like
+    evnt=evnt_mat['evnt']
+    mystery=utils.mat2dict(evnt)
+    # get using weird indexing because object ndarray
+    indexed_stim_nm=mystery['name'][0, stim_index][0]
+    block=indexed_stim_nm[:3].capitalize().replace('0','')
+    sync_position=mystery['syncPosition'][0,stim_index][0,0]
+    
+
+elif choose_timestamps=="mine":
+    tmstmps_pth=os.path.join("..","eeg_data","timestamps",subj_cat,subj_num,"timestamps.pkl")
+    with open(tmstmps_pth, 'rb') as f:
+        timestamps=pickle.load(f)
+    # flatten blocks so we can match flattened index
+    all_nms=[]
+    all_stmps=[]
+    for _, nms_n_stamps in timestamps.items():
+        for nm, stmps in nms_n_stamps.items():
+            all_nms.append(nm)
+            all_stmps.append(stmps)
+    indexed_stim_nm=all_nms[stim_index]
+    block=indexed_stim_nm[:3].capitalize().replace('0','')
+    sync_positions=all_stmps[stim_index]
+
+stim_wav=utils.get_stim_wav(stims_dict,indexed_stim_nm,'clean')
+# get eeg sound recording 
+stim_dur=(stim_wav.size-1)/fs_audio
+if choose_timestamps=="evnt":
+    eeg_wav=subj_eeg[block][sync_position:sync_position+int(stim_dur*fs_eeg+1),-1]
+elif choose_timestamps=="mine":
+    eeg_wav=subj_eeg[block][sync_positions[0]:sync_positions[1],-1]
+#%% 
+# plot    
+
 
 plt.figure()
 plt.plot(stim_wav)
