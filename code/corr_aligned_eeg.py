ee2a9963bf60527357f6c3284df77dc833b7755c
someSkunkFunk <dersfocused@gmail.com>
2024-04-18 17:54:25 -0400

    further edits to corr_aligned_eeg; needs to account for stims having different lengths in different subjects though I think

--- code/corr_aligned_eeg.py
+++ code/corr_aligned_eeg.py
@@ -18,11 +18,11 @@ def load_all_subj_data(evnt_thresh='000'):
     thresh_dir=f"thresh_{evnt_thresh}" # should be three digit number representing decimals to third place
     prep_data_dir=os.path.join("..","eeg_data","preprocessed_evnt",thresh_dir)
     if _limit_subjs:
-        amt_subjs=1
-        print("only loading a single subject. set _limit_subjs to False to load all subjects")     
+        _amt_subjs=2
+        print(f"only loading a {_amt_subjs} subject(s). set _limit_subjs to False to load all subjects")     
     else:
         amt_subjs=20 #number of subjs
-    subj_ints=[ii for ii in range(amt_subjs)]
+    subj_ints=[ii for ii in range(_amt_subjs)]
     all_subj_data={}
     for subj_int in subj_ints:
         subj_num=utils.assign_subj(subj_int)
@@ -37,7 +37,7 @@ def load_all_subj_data(evnt_thresh='000'):
 #%%
 # STEP 3: GROUP TIMESTAMPS INTO TRIALS BASED ON PAUSES
 # then recalculate onsets/offsets relative to start of trial?
-def split_eeg_trials_to_wavs(all_subj_data,evnt_nms,evnt_onsets,evnt_offsets):
+def split_eeg_trials_to_wavs(all_subj_data,fs):
     '''
     splits trials of eeg back into eeg for each individual wav file
     assumes evnt info specified in same fs as all_subj_data
@@ -48,7 +48,9 @@ def split_eeg_trials_to_wavs(all_subj_data,evnt_nms,evnt_onsets,evnt_offsets):
     '''
     eeg_by_wavs={}
     for subj, subj_data in all_subj_data.items():
-        print(f'splitting subj {subj} trials into individual stims')
+        print(f"loading timestamps for subject {subj}")
+        evnt=utils.load_evnt(subj)
+        evnt_nms,evnt_blocks,evnt_confidence,evnt_onsets,evnt_offsets=utils.extract_evnt_data(evnt,fs_trf)
         for trial_num, (_, data) in enumerate(subj_data.items()):
             print(f"starting trial {trial_num+1} of {len(subj_data)}.")
             nms_in_trial=data[0]
@@ -56,8 +58,11 @@ def split_eeg_trials_to_wavs(all_subj_data,evnt_nms,evnt_onsets,evnt_offsets):
             trial_start=evnt_onsets[evnt_nms==nms_in_trial[0]]
             for nm in nms_in_trial:
                 # get onset/offset relative to trial start
-                stim_trial_onset=(evnt_onsets[evnt_nms==nm]-trial_start)[0]
-                stim_trial_offset=(evnt_offsets[evnt_nms==nm]-trial_start)[0]
+                try:
+                    stim_trial_onset=(evnt_onsets[evnt_nms==nm]-trial_start)[0]
+                    stim_trial_offset=(evnt_offsets[evnt_nms==nm]-trial_start)[0]
+                except:
+                    pass
                 # note: indexing to get int instead of array 
                 if nm in eeg_by_wavs:
                     eeg_by_wavs[nm].append(eeg[stim_trial_onset:stim_trial_offset])
@@ -72,11 +77,20 @@ from scipy.stats import pearsonr
 def split_half_corr(eeg_by_wavs):
     corrs_by_wavs={}
     pvals_by_wavs={}
+    n_electrodes=62
     for stim_nm, all_subjs_eeg in eeg_by_wavs.items():
         n_subjs=len(all_subjs_eeg)
-        corr,pval=pearsonr(all_subjs_eeg[:n_subjs//2],all_subjs_eeg[n_subjs//2:])
-        corrs_by_wavs[stim_nm]=corr
-        pvals_by_wavs[stim_nm]=pval
+        
+        first_split=np.asarray(all_subjs_eeg[:n_subjs//2])
+        first_split_mean=first_split.mean(axis=0)
+        second_split=np.asarray(all_subjs_eeg[n_subjs//2:])
+        second_split_mean=second_split.mean(axis=0)
+        corrs_by_wavs[stim_nm]=np.zeros(n_electrodes)
+        pvals_by_wavs[stim_nm]=np.zeros(n_electrodes)
+        for ielec in range(n_electrodes):
+            corr,pval=pearsonr(first_split_mean[:,ielec],second_split_mean[:,ielec])
+            corrs_by_wavs[stim_nm][ielec]=corr
+            pvals_by_wavs[stim_nm][ielec]=pval
 
 
 
@@ -88,15 +102,12 @@ if __name__=='__main__':
 
     all_subj_data=load_all_subj_data()
     fs_trf=100 # evnt times in seconds; use trf sampling rate for preprocessed data
-    sample_subj=[k for k in all_subj_data.keys()][0]
-    print(f"loading timestamps for subject {sample_subj}")
-    evnt=utils.load_evnt(sample_subj)
-    evnt_nms,evnt_blocks,evnt_confidence,evnt_onsets,evnt_offsets=utils.extract_evnt_data(evnt,fs_trf)
-    eeg_by_wavs=split_eeg_trials_to_wavs(all_subj_data,evnt_nms,evnt_onsets,evnt_offsets)
+    sample_subjs=[k for k in all_subj_data.keys()]
+    eeg_by_wavs=split_eeg_trials_to_wavs(all_subj_data,fs=fs_trf)
     corrs_by_wavs,pvals_by_wavs=split_half_corr(eeg_by_wavs)
     
 
-
+ 
 
 #OLD IDEA:
     
