a6a34cefaa4a078749d6972d0c1df63306358f7a
someSkunkFunk <dersfocused@gmail.com>
2024-06-12 13:09:13 -0400

    made sure plot_lambda_curve imports data successfully

--- code/plot_lambda_curve.py
+++ code/plot_lambda_curve.py
@@ -9,16 +9,18 @@ import utils
 
 subjs_list=utils.get_all_subj_nums()
 #NOTE THRESH FOLDS DIR HARDCODED BUT THIS MAY BE CUMBERSOME IN FUTURE
-lambda_dir=os.path.joint("..","results","evnt_decimate",
+lambda_dir=os.path.join("..","results","evnt_decimate",
 "thresh_750_5fold_shuffled")
-
+noisy_or_clean="clean"
+rms_str="_rms"
+lambda_fnm=f"lambda_tuning_curve_{noisy_or_clean}{rms_str}.pkl"
 for subj_num in subjs_list:
     subj_cat=utils.get_subj_cat(subj_num)
     figs_dir=os.path.join("..","figures",
     "lambda_tuning_curve",subj_cat,subj_num)
     if not os.path.isdir(figs_dir):
-        os.makedirs(figs_dir,exist=True)
-    subj_lambda_pth=os.path.join(lambda_dir,subj_cat,subj_num)
+        os.makedirs(figs_dir,exist_ok=True)
+    subj_lambda_pth=os.path.join(lambda_dir,subj_cat,subj_num,lambda_fnm)
     with open(subj_lambda_pth, 'rb') as fl:
         subj_lambda=pickle.load(fl)
     print(f"{subj_num} loaded")
