25e14a7deb9a5098e71cdcf30cba7b0be6d8f2a1
someSkunkFunk <dersfocused@gmail.com>
2024-07-23 16:32:59 -0400

    extract_transcripts edits minor

--- code/extract_transcripts.py
+++ code/extract_transcripts.py
@@ -3,15 +3,22 @@
 import os
 import utils
 import numpy as np
-from string import punctuation
+from string import punctuation, digits
 import pickle
 
 stims_dict=utils.get_stims_dict()
 #%%
 # EXEC
-
-for sentence,nm in zip(stims_dict['String'],stims_dict['Name']):
-    story_nm=
+story_nms=[n.replace('./Sounds/','') for n in stims_dict["Name"]]
+story_nms=[n.replace('_16K_NM.wav','') for n in story_nms]
+story_nms=[n.replace('./Sounds/','') for n in stims_dict["Name"]]
+story_nms=[n.replace('_16K_NM.wav','') for n in story_nms]
+# generate 
+stories=set([n.rstrip(digits) for n in story_nms])
+ids=[x for x in stims_dict['ID']]
+story_stim_ids=[[nm,x] for nm,x in zip(story_nms,ids)]
+for sentence,nm in zip(stims_dict['String'],story_nms):
+    
     if isinstance(sentence,str):
         pass
     elif isinstance(sentence,np.ndarray):
