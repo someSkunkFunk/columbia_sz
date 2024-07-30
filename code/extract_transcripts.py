73faca19b80f813a42fe21b95779f7a6af4e0107
someSkunkFunk <dersfocused@gmail.com>
2024-07-30 11:49:22 -0400

    finished extract_transcripts, just need to save pkls

--- code/extract_transcripts.py
+++ code/extract_transcripts.py
@@ -30,7 +30,45 @@ def check_numbers_consistent(block_parts_tally:dict,exclude_stories=None):
             bool_shit.append(False)
     return all(bool_shit)
 # story_nms_detailed=[n.replace('./Sounds/','').replace('_16K_NM.wav','') for n in stims_dict["Name"]]
+def make_story_filter(story_stim_ids,exclude_stories={'hankFour','common'}):
+    '''
+    makes bool list of stories
+    assumes story_stim_ids in presentation order so no need to keep track of blocks
+    '''
+    keep_stories_filter=[False]*len(story_stim_ids)
+    mem={}
+    for ii, [story, stim_id] in enumerate(story_stim_ids):
+        if story in exclude_stories:
+            continue
+        # block,part=stim_id[1:3],stim_id[-2:]
+        part=stim_id[-2:]
+        if not story in mem:
+            #init a set
+            mem[story]={part}
+            keep_stories_filter[ii]=True
+        elif story in mem and part not in mem[story]:
+            mem[story].add(part)
+            keep_stories_filter[ii]=True
+    return keep_stories_filter
 
+def group_stories(stims_dict,story_nms,story_filter):
+    keep_strings=[(stnc,nm) for boo,stnc,nm in 
+    zip(story_filter,stims_dict['String'],story_nms) if boo]
+    # keep IDs to link back into correct trials:
+    keep_ids=stims_dict['ID'][story_filter]
+    
+    grouped_strings={'hank':[],'howOne':[],
+    'howTwo':[],'howThree':[],'howFour':[]}
+    for stnc,nm in keep_strings:
+        if isinstance(stnc,np.ndarray):
+            stnc=stnc[0] #extract string from array
+        # put sentences in proper list
+        if nm.startswith('hank'):
+            grouped_strings['hank'].append(stnc)
+        elif nm.startswith('how'):
+            grouped_strings[nm].append(stnc)
+
+    return grouped_strings,keep_ids
 
 # i learned how to use map function finally oooo...
 story_nms_detailed=list(map(lambda n: n.replace('./Sounds/','').replace('_16K_NM.wav',''), stims_dict["Name"]))
@@ -38,7 +76,6 @@ story_nms=list(map(lambda n: n.split("_")[1].rstrip(digits), story_nms_detailed)
 # generate set of unique stories since some are repeated with different speaker/noise but doesn't affect surprisal value:
 gender_stories=set([n.rstrip(digits) for n in story_nms_detailed])
 stories=set([nm.split('_')[1] for nm in gender_stories])
-exclude_stories={'common'} # since that one is weird on several accounts... although others not really in the clear...
 ids=[x for x in stims_dict['ID']]
 story_stim_ids=[[nm,x] for nm,x in zip(story_nms,ids)]
 # get number of parts for each story, also ensure the number is consistent across blocks:
@@ -52,21 +89,10 @@ for block in block_parts_tally:
         block_parts_tally[block][story]=len(list(filter(lambda x: x[1].startswith(block) and x[0]==story,story_stim_ids)))
 consistent_bool=check_numbers_consistent(block_parts_tally)
 print(f"numbers consistent? {consistent_bool}")
-# for [nm, stim_id] in story_stim_ids:
-
-#     block=stim_id[:3]
-#     block_parts_tally[block]
-
-
-# TODO: finish stuff below
-
-
-
-
-    
-    
-
-    
+story_filter=make_story_filter(story_stim_ids)
+for story_info,filter_value in zip(story_stim_ids,story_filter):
+    print(story_info,filter_value)
+grouped_transcripts=group_stories(stims_dict,story_nms,story_filter)
 
     
 
@@ -78,4 +104,6 @@ for sentence,nm in zip(stims_dict['String'],story_nms_detailed):
     if isinstance(sentence,str):
         pass
     elif isinstance(sentence,np.ndarray):
-        pass
+        print(sentence)
+        print(sentence[0])
+        print(type(sentence[0]))
